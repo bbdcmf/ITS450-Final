cdbf5c6e403caf71d8c9bb0ecdb47210d0e1073a
Ryan <freder20@pnw.edu>
2021-11-04 04:10:22 -0500

    Changes to be committed: 	modified:   css/project.css 	modified:   index.php 	Added grabbing from the database to the index.php - Formatting still needs work

--- index.php
+++ index.php
@@ -5,21 +5,13 @@ require('html/index.html');
 if(isset($_POST['searchBar'])){
 	echo($_POST['searchBar']);
 }
-echo("<tr>
-					<td>Product 1</td>
-					<td>Product 2</td>
-				</tr>
-					<td>Product 3</td>
-					<td>Product 4</td>
-				</tr>
-				<tr>
-					<td>Product 5</td>
-					<td>Product 6</td>
-				</tr>
-					<td>Product 7</td>
-					<td>Product 8</td>
-				</tr>
-			</table>
+
+$stmt = $db->query("SELECT * FROM shop");
+while($row = $stmt->fetch_assoc()){
+	echo("<tr><td class='shoptd'><img src='" . $row['imgPath'] . "' /><div class='shopItem'>" . "Product: " .  $row['item'] . " | Price: $" . $row['price'] .  " | Quantity: " . $row['quantity'] . " | Description: ". $row['description'] . "</div></td></tr>");
+}
+
+echo("		</table>
 		</div>
 	</body>
 </html>")

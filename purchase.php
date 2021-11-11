8f889e86796039db6c5e98e39854e142ffde6f82
Ryan <freder20@pnw.edu>
2021-11-10 18:55:35 -0600

    Fixed a small bug that arose after my last commit

--- purchase.php
+++ purchase.php
@@ -1,11 +1,13 @@
 <?php
 // TODO: Add a method for the user to purchase a product as well as search the DB for the vendors details for the payment to go to
-ob_start(); // need this for redirecting the user to a different page
+ob_start(); // Need this for redirecting the user to a different page
 require('header.php');
 require('html/purchase.html');
 
 // If the user is logged in
 if(isset($_SESSION['isLoggedInToLemonShop']) and $_SESSION['isLoggedInToLemonShop'] == true){
+	// Create the div where the product details are shown
+	echo("<div class='purchaseDiv'>");
 	// get the productID from the url
 	$productID = $_GET['id'];
 	// lookup the item from the DB
@@ -27,16 +29,23 @@ if(isset($_SESSION['isLoggedInToLemonShop']) and $_SESSION['isLoggedInToLemonSho
 	$row = $result->fetch_assoc();
 	$vendor = $row['username'];
 	
-	echo("Product: " . $productName . " Price: " . $price . " Description: " . $desc . " Vendor " . $vendor);
+	// Print all of the details
+	echo("Product: " . $productName . " Price: " . $price . " Description: " . $desc . " Vendor: " . $vendor);
 	echo('</div></body></html>');
 }
 // If the user is not logged in, send them to login.php
 else{
-	require('html/loggedIn.html'); // format the error
-    echo("<h3>You must login before purchasing a product.</h3>"); // Send the error before redirecting user to login
+	// Close purchase.html's page
+	echo('</body></html>');
+	
+	// Require loggedIn.html to format the error that is given
+	require('html/loggedIn.html');
+	// Send the error before redirecting user to login
+    echo("<h3>You must login before purchasing a product.</h3>");
 	$location = BASE_URL . "login.php";
 	$_SESSION['chosenID'] = $_GET['id'];
-    header( "Refresh:1; url=$location", true, 303); // redirect the user to login.php
+	// Redirect the user to login.php
+    header( "Refresh:1; url=$location", true, 303);
 	exit();
 }
 ?>

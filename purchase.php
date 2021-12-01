6d7a6ab1ee897870a72989fbd860bd08d8a141a3
Ryan <freder20@pnw.edu>
2021-12-01 01:48:05 -0600

    Changes to be committed: 	modified:   html/index.html 	modified:   purchase.php

--- purchase.php
+++ purchase.php
@@ -29,6 +29,34 @@ if(isset($_SESSION['isLoggedInToLemonShop']) and $_SESSION['isLoggedInToLemonSho
 	$row = $result->fetch_assoc();
 	$vendor = $row['username'];
 	
+	// Paypal scripts
+	echo('<script src="https://www.paypal.com/sdk/js?client-id=AQUDW2NzyMB0edExztPC5xJOvSd0N7MLT4uFP3QLEL2eXpH--Yzuis0BNhNBJ1bqd5pv-zIl8QvzFwuE"></script>
+
+  	<div id="paypal-button-container"></div>
+
+  	<script>
+  		paypal.Buttons({
+
+    			createOrder: function(data, actions) {
+      				// This function sets up the details of the transaction, including the amount and line item details.
+      				return actions.order.create({
+        				purchase_units: [{
+          					amount: {
+            						value: "' . $price . '"
+          					}
+        				}]
+      				});
+    			},
+    			onApprove: function(data, actions) {
+      				// This function captures the funds from the transaction.
+      				return actions.order.capture().then(function(details) {
+        				// This function shows a transaction success message to your buyer.
+        				alert("Transaction completed by " + details.payer.name.given_name);
+      				});
+    			}
+  		}).render("#paypal-button-container");
+  		//This function displays Smart Payment Buttons on your web page.
+	</script>');
 	// Print all of the details
 	echo("Product: " . $productName . " Price: " . $price . " Description: " . $desc . " Vendor: " . $vendor);
 	echo('</div></body></html>');

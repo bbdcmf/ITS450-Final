1f62d1bc87a127753cbf76c4de63728b5e3a15ac
Ryan <freder20@pnw.edu>
2021-12-03 23:29:18 -0600

    Changes to be committed: 	modified:   orders.php 	new file:   uploads/strawberry.jpg

--- orders.php
+++ orders.php
@@ -1,44 +1,54 @@
 <?php
-// TODO: Take the info from the database and send it to the 'View Past Orders' div.
-
 require('header.php');
+
+//If the user is logged in
+if(isset($_SESSION['isLoggedInToLemonShop']) and $_SESSION['isLoggedInToLemonShop'] == true){
+	// Get the user's userID from the database (used for SQL below)
+	$stmt = $db->prepare("SELECT id FROM users WHERE username=?");
+	$stmt->bind_param("s", $_SESSION['username']);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$user = $result->fetch_assoc();
+	$userID = $user['id'];
+
 // If the admin is checking the orders page
-if(isset($_SESSION['username']) and $_SESSION['username'] == 'vendor'){
-	require('html/ordersAdmin.html');
-	// Add the CSRF token by hashing the data 'orders.php' using SHA256 with the session token as the secret key
-	echo('			<input type="hidden" name="csrf-token" value="' . hash_hmac("sha256", "orders.php", $_SESSION["token"]) . '" />
-				<input type="submit" name="makeOrderSubmit" value="Submit" style="margin-left: 40%;" />
-			</form>
-		</div>');
-	$target_dir = "uploads/"; // The folder where the images will be stored in the webserver
-	$errorstr = 'Error: '; // Init. the error string in case there are errors
+
+	if(isset($_SESSION['username']) and $_SESSION['username'] == 'vendor'){
+		require('html/ordersAdmin.html');
+		// Add the CSRF token by hashing the data 'orders.php' using SHA256 with the session token as the secret key
+		echo('			<input type="hidden" name="csrf-token" value="' . hash_hmac("sha256", "orders.php", $_SESSION["token"]) . '" />
+					<input type="submit" name="makeOrderSubmit" value="Submit" style="margin-left: 40%;" />
+				</form>
+			</div>');
+		$target_dir = "uploads/"; // The folder where the images will be stored in the webserver
+		$errorstr = 'Error: '; // Init. the error string in case there are errors
 
 
-	// If the admin has pressed the 'Submit' button
-	if(isset($_POST['makeOrderSubmit']) and !empty($_POST['csrf-token'])){
-		// Hash the data 'orders.php' using SHA256 with the session token as the secret key
-		$token = hash_hmac('sha256', 'orders.php', $_SESSION['token']);
-		// If the CSRF token on our end does not equal the CSRF token they provided
-		if (!hash_equals($token, $_POST['csrf-token'])) {
-			$errorstr = $errorstr . 'Cross-site request forgery detected, ';
-		}
-		// If no image was selected
-		if($_FILES['image']['error']){
-			$errorstr = $errorstr . 'Please select an image, ';
-		}
-		// If an image was selected
-		else {
+		// If the admin has pressed the 'Submit' button
+		if(isset($_POST['makeOrderSubmit']) and !empty($_POST['csrf-token'])){
+			// Hash the data 'orders.php' using SHA256 with the session token as the secret key
+			$token = hash_hmac('sha256', 'orders.php', $_SESSION['token']);
+			// If the CSRF token on our end does not equal the CSRF token they provided
+			if (!hash_equals($token, $_POST['csrf-token'])) {
+				$errorstr = $errorstr . 'Cross-site request forgery detected, ';
+			}
+			// If no image was selected
+			if($_FILES['image']['error']){
+				$errorstr = $errorstr . 'Please select an image, ';
+			}
+			// If an image was selected
+			else {
 	
-			// Make sure the image is an actual image
-			$target_file = $target_dir . basename($_FILES["image"]["name"]);
-			$uploadOk = 1;
-			$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-	  		$check = getimagesize($_FILES["image"]["tmp_name"]);
+				// Make sure the image is an actual image
+				$target_file = $target_dir . basename($_FILES["image"]["name"]);
+				$uploadOk = 1;
+				$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		  		$check = getimagesize($_FILES["image"]["tmp_name"]);
   		
-	  		// If the image seems good:
-	  		if($check !== false) {
-	    		// Upload the image to the uploads/ folder
-	  			if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+		  		// If the image seems good:
+		  		if($check !== false) {
+		    		// Upload the image to the uploads/ folder
+		  			if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
 	  			} 
 	  			// If theres an error uploading the file, send the error
 	  			else {
@@ -107,36 +117,26 @@ if(isset($_SESSION['username']) and $_SESSION['username'] == 'vendor'){
 				$buyer = $result3->fetch_assoc();
 				echo("<tr><td class='pastOrderstd'><img src='" . $row['imgPath'] . "' /><div class='pastOrdersItem'>" . "Product: " .  $row['item'] . " | Price: $" . $row['price'] .  " | Quantity: " . $row['quantity'] . " | Description: ". $row['description'] . " | Buyer: " . $buyer['username'] . "</td></tr></div>");	
 			}
-			echo("</table>");
 		
-	echo("</div>
-</body>
-</html>");
-}
-
-// If a regular user is checking their orders
-else{
-	require('html/orders.html');
-	// Get the user's userID from the database (used for SQL below)
-	$stmt = $db->prepare("SELECT id FROM users WHERE username=?");
-	$stmt->bind_param("s", $_SESSION['username']);
-	$stmt->execute();
-	$result = $stmt->get_result();
-	$user = $result->fetch_assoc();
-	$userID = $user['id'];
-
-	$stmt = $db->prepare("SELECT * FROM orders WHERE userID = ?");
-	$stmt->bind_param("i", $userID);
-	$stmt->execute();
-	$result = $stmt->get_result();
-	while($order = $result->fetch_assoc()){
-		$stmt2 = $db->prepare("SELECT * FROM shop WHERE productID = ?");
-		$stmt2->bind_param("i", $order['productID']);
-		$stmt2->execute();
-		$result2 = $stmt2->get_result();
-		$row = $result2->fetch_assoc();
-		echo("<tr><td class='pastOrderstd'><img src='" . $row['imgPath'] . "' /><div class='pastOrdersItem'>" . "Product: " .  $row['item'] . " | Price: $" . $row['price'] .  " | Quantity: " . $row['quantity'] . " | Description: ". $row['description'] . "</div><a href='" . ENC_URL . "purchase.php?id=" . $row['productID'] . "' style='margin-left:40%;'/></td></tr>");	
+		echo("</table></div></body></html>");
 	}
-	echo("</table></div></body></html>");
+
+	// If a regular user is checking their orders
+	else{
+		require('html/orders.html');
+		$stmt = $db->prepare("SELECT * FROM orders WHERE userID = ?");
+		$stmt->bind_param("i", $userID);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		while($order = $result->fetch_assoc()){
+			$stmt2 = $db->prepare("SELECT * FROM shop WHERE productID = ?");
+			$stmt2->bind_param("i", $order['productID']);
+			$stmt2->execute();
+			$result2 = $stmt2->get_result();
+			$row = $result2->fetch_assoc();
+			echo("<tr><td class='pastOrderstd'><img src='" . $row['imgPath'] . "' /><div class='pastOrdersItem'>" . "Product: " .  $row['item'] . " | Price: $" . $row['price'] .  " | Quantity: " . $row['quantity'] . " | Description: ". $row['description'] . "</div><a href='" . ENC_URL . "purchase.php?id=" . $row['productID'] . "' style='margin-left:40%;'/></td></tr>");	
+	}
+		echo("</table></div></body></html>");
+	}
 }
 ?>

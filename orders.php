dae86e8c64c7a95d7a7ad320f7b9ba147678e1a9
Ryan <freder20@pnw.edu>
2021-12-04 00:50:39 -0600

    Changes to be committed: 	modified:   orders.php

--- orders.php
+++ orders.php
@@ -3,6 +3,13 @@ require('header.php');
 // If the admin is checking the orders page
 if(isset($_SESSION['username'], $_SESSION['isLoggedInToLemonShop']) and $_SESSION['username'] == 'vendor' and $_SESSION['isLoggedInToLemonShop'] == true){
 	require('html/ordersAdmin.html');
+	// Get the user's userID from the database (used for SQL below)
+	$stmt = $db->prepare("SELECT id FROM users WHERE username=?");
+	$stmt->bind_param("s", $_SESSION['username']);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$user = $result->fetch_assoc();
+	$userID = $user['id'];
 	// Add the CSRF token by hashing the data 'orders.php' using SHA256 with the session token as the secret key
 	echo('			<input type="hidden" name="csrf-token" value="' . hash_hmac("sha256", "orders.php", $_SESSION["token"]) . '" />
 				<input type="submit" name="makeOrderSubmit" value="Submit" style="margin-left: 40%;" />
@@ -119,14 +126,6 @@ echo("</div>
 // If a regular user is checking their orders
 else{
 	require('html/orders.html');
-	// Get the user's userID from the database (used for SQL below)
-	$stmt = $db->prepare("SELECT id FROM users WHERE username=?");
-	$stmt->bind_param("s", $_SESSION['username']);
-	$stmt->execute();
-	$result = $stmt->get_result();
-	$user = $result->fetch_assoc();
-	$userID = $user['id'];
-
 	$stmt = $db->prepare("SELECT * FROM orders WHERE userID = ?");
 	$stmt->bind_param("i", $userID);
 	$stmt->execute();
